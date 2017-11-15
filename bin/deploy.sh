136ed1c86745d914ea4d61033623168be1cc6e7e
Omar Sahyoun <osahyoun@gmail.com>
2017-11-15 18:00:13 +0000

    Invalidate CF on deploy

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -7,7 +7,13 @@ echo "Deploying static assets to production..."
 
 BUCKET_NAME=services.sumofus.org
 WEBSITE_URL=https://services.sumofus.org
+CF_DISTRIBUTION_ID=E2QLZWJLGWMLYK
 
 aws s3 sync --acl 'public-read' --delete ./build/ "s3://${BUCKET_NAME}/"
+
+aws cloudfront create-invalidation \
+  --distribution-id ${CF_DISTRIBUTION_ID} \
+  --paths '/*'
+
 
 echo "Deployed to ${WEBSITE_URL}"
