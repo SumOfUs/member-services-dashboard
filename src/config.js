a206cc6ba66d7fd52fbda508931d0738055f91b6
Omar Sahyoun <osahyoun@gmail.com>
2017-11-16 15:21:33 +0000

    Updates config with correct env var names

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
   cognito: {
-    USER_POOL_ID: process.env.COGNITO_USER_POOL_ID,
-    APP_CLIENT_ID: process.env.COGNITO_APP_CLIENT_ID,
+    USER_POOL_ID: process.env.REACT_APP_COGNITO_USER_POOL_ID,
+    APP_CLIENT_ID: process.env.REACT_APP_COGNITO_APP_CLIENT_ID,
   }
 };
