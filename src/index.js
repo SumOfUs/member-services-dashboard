dc68e9876e06172de6e75e03ab886f013c4ee466
Vince Martinez <eykosioux@gmail.com>
2017-11-23 17:25:39 +0000

    Add hot module reloading

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById("root")
-);
+function render(Component = App) {
+  ReactDOM.render(
+    <Router>
+      <Component />
+    </Router>,
+    document.getElementById('root')
+  );
+}
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    const NextApp = require('./App').default;
+    render(NextApp);
+  });
+}
+
+render();
 
 registerServiceWorker();
